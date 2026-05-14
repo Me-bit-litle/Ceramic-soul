64caf373b9fbf3603dab74cba570638c908c7c46
academyHH <ileayy201@gmail.com>
2026-05-14 16:15:29 +0300

    finish work with burger-menu

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,20 @@ import 'swiper/css/pagination';
 
 import "../sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+	close = document.querySelector(".header__menu-close"),
+	menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+	menu.classList.add("header__menu_active");
+	document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+	menu.classList.remove("header__menu_active");
+	document.body.style.overflow = "";
+});
+
 const swiper = new Swiper(".works__slider", {
     modules: [Navigation, Pagination],
     slidesPerView: 1,
