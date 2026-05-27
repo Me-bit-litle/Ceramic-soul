942c13fd2f2033e6945e6603d29e2430e1d8e196
academyHH <ileayy201@gmail.com>
2026-05-27 18:35:27 +0300

    the plagin problem

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,39 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				catalog: resolve(__dirname, "catalog.html"),
+				catalog: "catalog.html",
+				blog: "blog.html",
+				about: "about.html",
 			},
 		},
 	},
+	plugins: [
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false,
+			},
+			optipng: {
+				optimizationLevel: 7,
+			},
+			mozjpeg: {
+				quality: 70,
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: "removeViewBox",
+					},
+					{
+						name: "removeEmptyAttrs",
+						active: false,
+					},
+				],
+			},
+		}),
+	],
 });
